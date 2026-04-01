113e506dc831291bbf30b188787639b635768c6e
Nguyễn Ngọc Trần Bách <seals@BachNNT.local>
2026-04-01 17:07:30 +0700

    full CRUD

--- BE/src/main/java/org/example/be/controller/ChiTietSanPhamController.java
+++ BE/src/main/java/org/example/be/controller/ChiTietSanPhamController.java
@@ -3,12 +3,11 @@ package org.example.be.controller;
 import jakarta.validation.Valid;
 import org.example.be.dto.*;
 import org.example.be.entity.ChiTietSanPham;
-import org.example.be.entity.Product;
 import org.example.be.entity.Sanpham;
 import org.example.be.exception.BeerResponseException;
-import org.example.be.repository.BookRepository;
 import org.example.be.repository.ChiTietSanPhamRepository;
 import org.example.be.repository.SanPhamRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,8 +27,7 @@ public class ChiTietSanPhamController {
     // lấy hết (các file liên quan: ChiTietSanPhamRepository, ChiTietSanPhamResponseDTO và file controller này )
     @GetMapping
     public ArrayList getAllChiTietSanPham() {
-        ArrayList<ChiTietSanPhamResponseDTO> lstChitiet =
-                (ArrayList<ChiTietSanPhamResponseDTO>) chiTietSanPhamRepository.getAllChiTietSanPham();
+        ArrayList<ChiTietSanPhamResponseDTO> lstChitiet = (ArrayList<ChiTietSanPhamResponseDTO>) chiTietSanPhamRepository.getAllChiTietSanPham();
         return lstChitiet;
     }
 
@@ -47,9 +45,7 @@ public class ChiTietSanPhamController {
 
         // tìm Sản phẩm theo trường sanphamid trong request
         // ở đây thầy dùng tạm beerResponseException, anh em viết 1 file exception tuỳ ý nhé
-        Sanpham sanpham =
-                sanphamRepository.findById(chiTietSanPhamRequestDTO.getSanphamid()).orElseThrow(
-                        () -> new BeerResponseException("Không tìm thấy sản phẩm rồi"));
+        Sanpham sanpham = sanphamRepository.findById(chiTietSanPhamRequestDTO.getSanphamid()).orElseThrow(() -> new BeerResponseException("Không tìm thấy sản phẩm rồi"));
 
         // set trường Sản phẩm trong Chitietsanpham đang tạo bằng Sản phẩm tìm được ở trên
         chitiet.setSanpham(sanpham);
@@ -58,9 +54,32 @@ public class ChiTietSanPhamController {
 
         // trả response
         // Nhớ tạo toString ở SanPham và ChiTietSanPham (thư mục Entity)
-        return ResponseEntity.ok(
-                new BeerResponseDTO("200",
-                        "Đã lưu chi tiết sản phẩm mới có thông tin là " + chitiet));
+        return ResponseEntity.ok(new BeerResponseDTO("200", "Đã lưu chi tiết sản phẩm mới có thông tin là " + chitiet));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable("id") Integer id) {
+        // tìm chi tiết sản phẩm xong xoá
+        ChiTietSanPham chitiet = chiTietSanPhamRepository.findById(id).orElseThrow(() -> new BeerResponseException("Không tìm thấy cái cần xoá"));
+        chiTietSanPhamRepository.delete(chitiet);
+        return ResponseEntity.ok(new BeerResponseDTO("200", "Đã xoá chi tiết sản phẩm " + chitiet));
+    }
+
+    // luồng update, update bản ghi chi tiết sản phẩm
+    @PutMapping("/update/{id}")
+    public ResponseEntity update(@PathVariable("id") Integer id, @Valid @RequestBody ChiTietSanPhamRequestDTO chiTietSanPhamRequestDTO) {
+        // Tìm bản ghi chi tiết sản phẩm cần sửa theo id
+        ChiTietSanPham chitiet = chiTietSanPhamRepository.findById(id).orElseThrow(() -> new BeerResponseException("Tao không tìm thấy nó"));
+
+        // Ghi đè thằng request lên thằng sửa,
+        // không ghi đè trường id để tránh bị hiểu lầm là tạo mới
+
+        BeanUtils.copyProperties(chiTietSanPhamRequestDTO, chitiet, "id");
+        // lưu
+        chiTietSanPhamRepository.save(chitiet);
+
+        // trả response
+        return ResponseEntity.ok(new BeerResponseDTO("200", "Đã update chi tiết sản phẩm " + chitiet));
     }
 
 
